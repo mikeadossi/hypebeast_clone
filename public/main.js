fe9a9776ecf7cc08e16cb81b9eb4a3b717ddb353
mikeadossi <mikeadossi@gmail.com>
2017-09-09 21:09:37 -0700

    complete nav dropdown items

--- public/main.js
+++ public/main.js
@@ -14,6 +14,18 @@ $('.close').on('click', () => {
   $('.magnifierMobile').css('display','inline-block');
 })
 
+// $('.search_container').css('display','none');
+
+$('.magnifier').on('click', () => {
+  $('.search_nav_container').css('display','block');
+})
+
+$('.search_nav_close').on('click', () => {
+  $('.search_nav_container').css('display','none');
+})
+
+
+
 $('.navAccountContainer').hover(
   () => {
     $('.accountDropDown').css('display','block');
@@ -27,5 +39,67 @@ $('.navGlobeContainer').hover(
     $('.globeDropDown').css('display','block');
   }, () => {
     $('.globeDropDown').css('display','none');
+  }
+)
+
+/* Nav bar hover behaviour */
+
+$('.fashionNavLink').hover(
+  () => {
+    $('.fashionNavLink').css('background-color','#1E1E1E')
+    $('.fashionNavLink').css('color','white')
+    $('.fashion_dropdown').css('display','block');
+  }, () => {
+    $('.fashionNavLink').css('background-color','unset')
+    $('.fashionNavLink').css('color','black')
+    $('.fashion_dropdown').css('display','none');
+  }
+)
+
+$('.footwearNavLink').hover(
+  () => {
+    $('.footwearNavLink').css('background-color','#1E1E1E')
+    $('.footwearNavLink').css('color','white')
+    $('.footwear_dropdown').css('display','block');
+  }, () => {
+    $('.footwearNavLink').css('background-color','unset')
+    $('.footwearNavLink').css('color','black')
+    $('.footwear_dropdown').css('display','none');
+  }
+)
+
+$('.musicNavLink').hover(
+  () => {
+    $('.musicNavLink').css('background-color','#1E1E1E')
+    $('.musicNavLink').css('color','white')
+    $('.music_dropdown').css('display','block');
+  }, () => {
+    $('.musicNavLink').css('background-color','unset')
+    $('.musicNavLink').css('color','black')
+    $('.music_dropdown').css('display','none');
+  }
+)
+
+$('.artsNavLink').hover(
+  () => {
+    $('.artsNavLink').css('background-color','#1E1E1E')
+    $('.artsNavLink').css('color','white')
+    $('.arts_dropdown').css('display','block');
+  }, () => {
+    $('.artsNavLink').css('background-color','unset')
+    $('.artsNavLink').css('color','black')
+    $('.arts_dropdown').css('display','none');
+  }
+)
+
+$('.allSectionsNavLink').hover(
+  () => {
+    $('.allSectionsNavLink').css('background-color','#1E1E1E')
+    $('.allSectionsNavLink').css('color','white')
+    $('.all_sections_dropdown').css('display','block');
+  }, () => {
+    $('.allSectionsNavLink').css('background-color','unset')
+    $('.allSectionsNavLink').css('color','black')
+    $('.all_sections_dropdown').css('display','none');
   }
 )
