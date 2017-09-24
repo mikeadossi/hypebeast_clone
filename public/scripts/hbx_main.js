260b683437fbf4429759141db000ff4a948f339e
mikeadossi <mikeadossi@gmail.com>
2017-09-23 20:24:12 -0700

    implement slider carousel for product images

--- public/scripts/hbx_main.js
+++ public/scripts/hbx_main.js
@@ -18,3 +18,67 @@ $( window ).resize(function() {
     $( '.hbx_mobile_menu_background' ).css( "display","none" );
   }
 });
+
+// let displayMe = function(image_to_display){
+//   $('.product_on_display').css("background-image",image_to_display);
+// }
+
+
+// $('select').on('change', function() {
+//   let href = window.location.href;
+//   href = href.split('/')
+//   console.log('href: ',href);
+//   let this_brand = href.pop();
+//   href = href.join('/');
+//   href = href+'/'+this_brand;
+//   console.log('href popped: ',href);
+//
+//   if(this.value === 'high_to_low'){
+//     console.log(href+'?sort=desc');
+//     window.location=href+'?sort=desc';
+//   } else if(this.value === 'low_to_high'){
+//     console.log(href+'?sort=asc');
+//     window.location=href+'?sort=asc';
+//   } else if(this.value === 'newest_first'){
+//     console.log(href);
+//     window.location=href;
+//   }
+//
+// })
+
+
+
+let product_container = $(".product_container");
+let product_on_display_container = $(".product_on_display_container")[0];
+let product_preview_container = $(".product_preview_container")[0];
+
+let product_preview_img;
+let num_of_images = $(".product_preview_img").length;
+let percent;
+
+for(let i = 0; i < num_of_images; i++){
+  product_preview_container.addEventListener("click", function(e) {
+    product_preview_img = $(".product_preview_img")[i];
+    if(e.target.parentElement === product_preview_img){
+      console.log('e.target.parentElement -> ',e.target.parentElement);
+      percent = i*100
+      $(".product_on_display").css("right",percent+"%");
+      $(".product_on_display").css("transition","1s");
+    }
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
